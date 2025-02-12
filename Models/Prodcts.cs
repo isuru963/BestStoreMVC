11f59f242079daa62617d2c84f809fe35c9cac52
Isuru S. Aththanayake <118103842+isuru963@users.noreply.github.com>
2025-02-12 23:30:10 +0530

    Add project files.

---
+++ Models/Prodcts.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BestStoreMVC.Models
+{
+    public class Prodcts
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+
+        [MaxLength(100)]
+        public string Brand { get; set; } = "";
+
+        [Precision(16, 2)]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = "";
+
+        [MaxLength(100)]
+        public string ImageFileName { get; set; } = "";
+
+    }
+}
---
+++ Services/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore; 
+
+namespace BestStoreMVC.Services
+
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
